c2772d57df46d5742de27e7961f9f427d3489254
Daniel Andrade <andradedanielin@gmail.com>
2023-04-23 16:23:44 -0500

    Setup de Proyecto Final

--- ProyectoFinal/main.cpp
+++ ProyectoFinal/main.cpp
@@ -2,13 +2,19 @@
 //  main.cpp
 //  ProyectoFinal
 //
-//  Created by Daniel Andrade on 23/4/23.
 //
 
 #include <iostream>
+using namespace std;
+
+struct tPolimonio {
+    int grado;
+    vector<double> coef;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
     return 0;
 }
+
