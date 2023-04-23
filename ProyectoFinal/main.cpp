0fee8520216aa1f4697543f797f34d5c1b50a62a
Daniel Andrade <andradedanielin@gmail.com>
2023-04-23 15:06:17 -0500

    Initial Commit

---
+++ ProyectoFinal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ProyectoFinal
+//
+//  Created by Daniel Andrade on 23/4/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
